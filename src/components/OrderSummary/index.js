e7abaad7b64095af5545d957bc6355b7964a10d0
vcorreia <vcorreia@pyx4.com>
2023-03-17 16:36:30 -0400

    add orderSummary

---
+++ src/components/OrderSummary/index.js
@@ -0,0 +1,28 @@
+import './OrderSummaryStyles.css'
+
+import React from 'react'
+
+export const OrderSummary = () => {
+    return (
+      <aside>
+        <h2>Order summary</h2>
+        <hr />
+
+        <p>
+          total miles
+          <span>2000</span>
+        </p>
+        <p>
+          GST/HST
+          <span>$20.00</span>
+        </p>
+        <hr />
+
+        <p>
+          Total cost
+          <span>$197.00</span>
+        </p>
+
+      </aside>
+    )
+}
